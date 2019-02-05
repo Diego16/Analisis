720eb519b394ae4d02587d98442a7cdf1c14bd95
Diego Guevara <diegogb0113@gmail.com>
2019-02-05 11:38:07 -0500

    Missing measures

---
+++ Main.java
@@ -0,0 +1,52 @@
+
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n;
+		System.out.println("Ingrese el valor de n para generar las matrices\n");
+		n = sc.nextInt();		
+		//Creacion de matrices		
+		int[][] m1 = new int[n][n];
+		int[][] m2 = new int[n][n];		
+		int[][] sol = new int [n][n]; 			
+		m1 = LlenarMatriz(m1, n);
+		Imprimir(m1, n);
+		m2 = LlenarMatriz(m2, n);		
+		Imprimir(m2, n);
+		sol = MultiplicarMatrices(m1, m2, n, sol);
+		Imprimir(sol, n);
+		sc.close();
+	}	
+	public static int[][] LlenarMatriz(int[][] mat, int n) {
+		for(int x = 0; x < n; x++) {
+			for(int y = 0; y < n; y++) {
+				int randomNum = ThreadLocalRandom.current().nextInt(0, 6);
+				mat[x][y] = randomNum;
+			}
+		}
+		return mat;
+	}
+	public static int[][] MultiplicarMatrices(int[][]m1, int [][]m2, int n, int[][]sol){
+		for(int x = 0; x < n; x ++) {
+			for(int y = 0; y < n; y++) {
+				sol[x][y] = 0;
+				for(int z = 0; z < n; z++) {
+					sol[x][y] += m1[x][z]* m2[z][y];
+				}
+			}
+		}
+		return sol;
+	}
+	public static void Imprimir(int[][]sol, int n){
+		for(int x = 0; x < n; x ++) {
+			for(int y = 0; y < n; y++) {
+				System.out.print(sol[x][y] + " ");
+			}
+			System.out.print("\n");
+		}
+		System.out.print("\n\n");
+	}
+}
